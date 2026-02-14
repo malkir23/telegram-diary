77897a24f7b964a9225c2340aa0c9237edda8168
Konstantin <mandragorand@gmail.com>
2026-02-14 18:20:26 +0000

    fix async db

--- alembic/env.py
+++ alembic/env.py
@@ -37,6 +37,7 @@ async def run_migrations_online() -> None:
         config.get_section(config.config_ini_section, {}),
         prefix="sqlalchemy.",
         poolclass=pool.NullPool,
+        connect_args={"statement_cache_size": 0, "prepared_statement_cache_size": 0},
     )
 
     async with connectable.connect() as connection:
